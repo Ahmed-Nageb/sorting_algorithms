9392d484d822fd98caa2e2116b147c457106df7c
Fatma Khalil <folla@192.168.1.9>
2024-05-01 00:45:47 +0300

    update

---
+++ 3-quick_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Function that sorts an array of integers in ascending
+ * order using the Quick sort
+ * @array: array of ints
+ * @size: size of array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+    size_t i, j, k;
+    int temp;
+
+    if (!array || !size)
+    {
+        return;
+    }
+    for (i = 0; i < size; i++)
+    {
+        for (j = size - 1, k = i + 1; j > i; j--)
+        {
+            if (array[j] < array[k])
+            {
+                k = j;
+            }
+        }
+        if(array[i] > array[k])
+        {
+            temp = array[i];
+            array[i] = array[k];
+            array[k] = temp;
+            print_array(array, size);
+        }
+    }
+}
